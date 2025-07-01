9bfa89392bdf459e7efe8c05e6b7ec1a63bcc888
jenn-jk <jenkins051@gmail.com>
2025-07-01 21:13:37 +0530

    created an empty scaffold

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TipCalc extends StatefulWidget {
+  @override
+  State<TipCalc> createState() {
+    return _TipCalcState();
+  }
+}
+
+class _TipCalcState extends State<TipCalc> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tipcalc/app.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: null,
+      home: TipCalc(),
     );
   }
 }
