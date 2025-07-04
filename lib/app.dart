147799e3b88fb21df9434a39ed1da50c77bab2ff
jenn-jk <jenkins051@gmail.com>
2025-07-04 18:13:06 +0530

    added theme and copywith

--- lib/app.dart
+++ lib/app.dart
@@ -10,17 +10,34 @@ class TipCalc extends StatefulWidget {
 class _TipCalcState extends State<TipCalc> {
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+
+    // add style
+    final style = theme.textTheme.titleMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+      fontWeight: FontWeight.bold,
+    );
+
     return Scaffold(
       appBar: AppBar(),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
             padding: EdgeInsets.all(10.0),
             decoration: BoxDecoration(
-              color: Colors.deepPurple,
+              color: Theme.of(context).primaryColor,
               borderRadius: BorderRadius.circular(10.0),
             ),
-            child: Column(children: [Text("Total per Person"), Text('\$20.0')]),
+            child: Column(
+              children: [
+                Text("Total per Person", style: style),
+                Text('\$20.0', style: style.copyWith(
+                  color: theme.colorScheme.onPrimary,
+                  fontSize: theme.textTheme.displaySmall!.fontSize
+                )),
+              ],
+            ),
           ),
         ],
       ),
